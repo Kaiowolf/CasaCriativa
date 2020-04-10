3872068838a649f5d9840ef5d796e3ac02ce2cb6
kaiowolf <kaioexpert@hotmail.com>
2020-04-10 17:45:26 -0300

    Finalizando Projeto
    Projeto Casa Criativa finalizado.
    Botão de deletar ainda não funcional, mas incluso.

--- public/scripts.js
+++ public/scripts.js
@@ -12,3 +12,39 @@ function onOff() {
   .classList
   .toggle('addScroll')
 }
+
+function checkFields(event) {
+
+  const valuesToCheck = [
+
+    "title",
+    "category",
+    "image",
+    "description",
+    "link",
+
+  ]
+
+  //trim tira espaços de ums string
+
+
+  const isEmpty = valuesToCheck.find(function(value) {
+    
+    const checkIfIsString = typeof event.target[value].value === "string";
+    const chekIfIsEmpty = !event.target[value].value.trim();
+
+    if(checkIfIsString && chekIfIsEmpty) {
+      return true
+    }
+  })
+
+  if(isEmpty) {
+    event.preventDefault()
+    alert('Por favor, preencha todos os campos.')
+  }
+}
+
+function buttonDelete () {
+
+  document.querySelector("body").classList.toggle("Delete")
+}
